3b7905c1fca003a85217a1842e105e50de248017
Andrey Barchuk <mazafakand@gmail.com>
2016-11-22 14:33:56 +0200

    hard

---
+++ app/js/add_project.js
@@ -0,0 +1,17 @@
+var myModule = (function () {
+
+	var init = function () {
+		_setUpListners();
+	};
+
+	var _setUpListners = function () {
+		//прослушка событий
+	}
+
+	return {
+		init: init
+	};
+	
+})();
+
+myModule.init();
